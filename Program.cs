9ea00776c8f96b1c6794728f838109d41fb716e5
ayushsrivastava7619 <shivamsrivastava23112001@gmail.com>
2023-08-25 09:42:32 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+class Employee
+{
+
+    public string Name;
+    public int Age;
+    public double Salary;
+
+    
+    public Employee(string name, int age, double salary)
+    {
+        Name = name;
+        Age = age;
+        Salary = salary;
+    }
+
+    
+    public void DisplayDetails()
+    {
+        Console.WriteLine($"Name: {Name}");
+        Console.WriteLine($"Age: {Age}");
+        Console.WriteLine($"Salary: {Salary}"); 
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        
+        Employee emp = new Employee("John Doe", 30, 50000.0);
+
+        
+        emp.DisplayDetails();
+    }
+}
